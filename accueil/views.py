e19c50ef1ad36c03955d6c8a717ebfbee40e7722
Helene Leblanc <h.leblanc.dev@gmail.com>
2020-01-18 21:15:09 +0100

    gestion de la connexion et redirection vers accueil admin ou accueil user en fonction du groupe de la personne qui se connecte

---
+++ accueil/views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import LoginForm
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def accueil(request):
+    if request.user.groups.filter(name="administrateur").exists():
+        return render(request, 'accueil/accueilAdmin.html')
+    else:
+        return render(request, 'accueil/accueilUser.html')
+
+
+def connect(request):
+    error = False
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('accueil')
+            else:
+                error = True
+    else:
+        form = LoginForm()
+
+    return render(request, 'accueil/login.html', locals())
+
+
+def disconnect(request):
+    logout(request)
+    return HttpResponseRedirect('logout/')
